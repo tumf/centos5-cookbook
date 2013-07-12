6d04685347a87285a354c5ed23f52cc3132b645f
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2013-07-12 21:59:49 +0900

    default 要らないやつ lvm2-monitor を lvm2に

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,6 @@ default['network-settings']['hostname'] = "localhost.localdomain"
 
 default["stop-services"] = %w[acpid auditd autofs avahi-daemon avahi-dnsconfd
  bluetooth conman cpuspeed cups dnsmasq dund gpm hidd
- ip6tables irda lvm2-monitor mcstrans mdmonitor multipathd
+ ip6tables irda lvm2 mcstrans mdmonitor multipathd
  pand pcscd psacct rawdevices readahead_early readahead_later
  restorecond saslauthd smartd wpa_supplicant ypbind yum-updatesd]
